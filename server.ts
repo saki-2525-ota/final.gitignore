9c2a67db98300deee39f67a16210889b4ffc8386
saki-2525-ota <206808265+saki-2525-ota@users.noreply.github.com>
2026-01-08 15:29:10 +0900

    変更

--- server.ts
+++ server.ts
@@ -144,7 +144,7 @@ const app = new Application();
 
 app.use(async (ctx, next) => {
   ctx.response.headers.set('Access-Control-Allow-Origin', '*');
-  ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST,  OPTIONS');
   ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
 
   if (ctx.request.method === 'OPTIONS') {
